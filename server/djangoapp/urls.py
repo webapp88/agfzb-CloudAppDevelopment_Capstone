1b79df267a34a49cd870846def184f9ef9d424c9
“webapp88” <dahuadata@gmail.com>
2022-10-04 20:42:49 -0400

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,10 +14,13 @@ urlpatterns = [
     # path for contact us view
 
     # path for registration
+    path(route='registration/', view=views.registration_request, name='registration'), 
 
     # path for login
+    path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
     path(route='about/', view=views.about, name='about'),
